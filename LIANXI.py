7a5a1b86116dac71916407c903fa048b19aca516
zhuixingjian-cpu <zhuixingjian@gmail.com>
2025-11-23 15:08:53 +0800

    Add files via upload

---
+++ LIANXI.py
@@ -0,0 +1,7 @@
+# print('|{:-<10}|'.format('曾贤志'))
+# print('|{:□<10}|'.format('曾贤志'))
+# print('|{:>10}|'.format('曾贤志'))
+# print('|{:□>10}|'.format('曾贤志'))
+# print('|{:□^10}|'.format('曾贤志'))
+import pyautogui
+print(pyautogui.__version__)
